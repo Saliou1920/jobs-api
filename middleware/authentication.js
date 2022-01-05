13c7514de936e3b4d0da9afe062c81ab474f7d75
Saliou1920 <diopsaliou45@gmail.com>
2022-01-05 17:50:32 -0400

    auth middleware

---
+++ middleware/authentication.js
@@ -0,0 +1,24 @@
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const { UnauthenticatedError } = require("../errors");
+
+const auth = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    throw new UnauthenticatedError("Invalid token");
+  }
+  const token = authHeader.split(" ")[1];
+  try {
+    const { id, name } = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(id).select("-password");
+    if (!user) {
+      throw new UnauthenticatedError("Invalid token");
+    }
+    req.user = user;
+    next();
+  } catch (err) {
+    throw new UnauthenticatedError("Invalid token");
+  }
+};
+
+module.exports = auth;
