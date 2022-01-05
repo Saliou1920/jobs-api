9db1742b8086574e7b26f9a438de9ef7fc4a5eb0
Saliou1920 <diopsaliou45@gmail.com>
2022-01-05 03:13:35 -0400

    register logic

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,33 @@
 const User = require("../models/User");
-const { StatusCodes } = (require = "http-status-codes");
+const { StatusCodes } = require("http-status-codes");
+const { BadRequestError, UnauthenticatedError } = require("../errors");
 
 const register = async (req, res) => {
-  const user = await User.create({ ...req.body });
-  res.status(StatusCodes.CREATED).json(user);
+  const { name, email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user) {
+    throw new BadRequestError("User already exists");
+  }
+  const newUser = await User.create({ name, email, password });
+  const token = newUser.createJWT();
+  res.status(StatusCodes.CREATED).json({ user: { name: newUser.name }, token });
 };
 
 const login = async (req, res) => {
-  res.send("login");
+  const { email, password } = req.body;
+  if (!email || !password) {
+    throw new BadRequestError("Email and password are required");
+  }
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new UnauthenticatedError("Invalid email or password");
+  }
+  //   const isValidPassword = await user.comparePassword(password);
+  //   if (!isValidPassword) {
+  //     throw new UnauthenticatedError("Invalid email or password");
+  //   }
+  const token = user.createJWT();
+  res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
 };
 
 module.exports = {
