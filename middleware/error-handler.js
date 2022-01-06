aa2058bf71d035a46f883c771645f9eb235e885f
Saliou1920 <diopsaliou45@gmail.com>
2022-01-06 00:03:20 -0400

    handling castError

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,10 +1,21 @@
-const { CustomAPIError } = require('../errors')
-const { StatusCodes } = require('http-status-codes')
+const { CustomAPIError } = require("../errors");
+const { StatusCodes } = require("http-status-codes");
 const errorHandlerMiddleware = (err, req, res, next) => {
   if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
+    return res.status(err.statusCode).json({ msg: err.message });
   }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
-}
+  if (err.name === "ValidationError") {
+    const messages = Object.values(err.errors).map((error) => error.message);
+    return res.status(StatusCodes.BAD_REQUEST).json({ msg: messages });
+  }
 
-module.exports = errorHandlerMiddleware
+  if (err.name === "CastError") {
+    return res.status(StatusCodes.BAD_REQUEST).json({ msg: "Invalid ID" });
+  }
+
+  return res
+    .status(StatusCodes.INTERNAL_SERVER_ERROR)
+    .json({ msg: "Internal server error" });
+};
+
+module.exports = errorHandlerMiddleware;
