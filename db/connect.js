fc04c0d0d1f7e2619c431f356c6cd35d72df1060
Saliou1920 <diopsaliou45@gmail.com>
2022-05-14 18:00:59 -0300

    add azure db name

--- db/connect.js
+++ db/connect.js
@@ -1,12 +1,13 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const connectDB = (url) => {
   return mongoose.connect(url, {
+    dbName: process.env.DB_NAME,
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
-  })
-}
+  });
+};
 
-module.exports = connectDB
+module.exports = connectDB;
